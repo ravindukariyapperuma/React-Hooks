b31c2a6972ae8b984685b72f8f79186c9c94e577
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-07-19 18:35:58 +0530

    HookCounterFour

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
 import HookCounterTwo from './components/HookCounterTwo'
 import HookCountThree from './components/HookCounterThree'
+import HookCounterFour from './components/HookCounterFour'
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
-      <HookCountThree />
+      {/* <HookCountThree /> */}
+      <HookCounterFour />
     </div>
   );
 }
